0aca2c3fabccf9b72c35332f2972bf5fa87c369c
andrewfriday13 <110158654+andrewfriday13@users.noreply.github.com>
2023-05-30 00:24:53 +0200

    remove space

--- models/user.js
+++ models/user.js
@@ -23,7 +23,7 @@ const userSchema = new Schema(
     token: {
       type: String,
       required: true,
-      default: " ",
+      default: "",
     },
   },
   { versionKey: false, timestamps: true }
